c1a225942fbc620fed034fb169864f5b98c983fb
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-12-13 12:15:54 +0400

    расширяющий метод, возвращающий инверсию точки относительно поля 8х8

---
+++ Lab2CheckersClient/PointExtention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Lab2CheckersClient
+{
+    public static class PointExtention
+    {
+        public static Point GetPointInvers(this Point point)
+        {
+            return new Point(Math.Abs(point.X - 7), Math.Abs(point.Y - 7));
+        }
+    }
+}
