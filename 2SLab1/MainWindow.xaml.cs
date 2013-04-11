ce8062d124c6aa112f47db4118f937ed216d5b26
Дмитрий Ключников <klyuchnikovds@gmail.com>
2013-04-11 13:22:01 +0400

    change the project to work with proxy Polytechnic

--- 2SLab1/MainWindow.xaml.cs
+++ 2SLab1/MainWindow.xaml.cs
@@ -41,6 +41,7 @@ namespace _2SLab1
             public string URL;
         }
         private byte[] buff;
+        const string proxyAuth = "Proxy-Authorization:Basic ZC5rbHl1Y2huaWtvdjozNzc0MDc=\r\n";
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -54,18 +55,19 @@ namespace _2SLab1
                 buff = new byte[1024];
                 Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 var uri = new Uri(textBox2.Text);
-
-                Sock.Connect(uri.Host, 80);
-                Sock.Send(Encoding.UTF8.GetBytes("GET " + uri.AbsolutePath + " HTTP/1.1\r\nHost:" + uri.Host + "\r\n\r\n"));
+                Sock.Connect("proxy.ustu", 3128);
+                //Sock.Connect(uri.Host, 80);
+                Sock.Send(Encoding.Default.GetBytes("CONNECT users.ulstu.ru HTTP/1.0\r\n"));
+                Sock.Send(Encoding.Default.GetBytes(string.Format("GET {0} HTTP/1.1\r\nHost:{1}\r\n{2}\r\n", uri.AbsolutePath, uri.Host, proxyAuth)));
+                //  Sock.Send(Encoding.Default.GetBytes("GET / HTTP/1.0\r\nHost:users.ulstu.ru\r\n\r\n" + proxyAuth));
 
                 // Create the state object.
-                StateObject state = new StateObject();
-                state.workSocket = Sock;
-                state.URL = textBox2.Text;
+                var state = new StateObject { workSocket = Sock, URL = textBox2.Text };
+                Sock.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                 new AsyncCallback(ReceiveCallback), state);
 
                 // Begin receiving the data from the remote device.
-                Sock.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                  new AsyncCallback(ReceiveCallback), state);
+
             }
             catch (Exception ex)
             {
@@ -75,14 +77,27 @@ namespace _2SLab1
 
         public void ReceiveCallback(IAsyncResult ar)
         {
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket client = state.workSocket;
+            var state = (StateObject)ar.AsyncState;
+            var client = state.workSocket;
             int bytesRead = client.EndReceive(ar);
             try
             {
-
-                var ss = Encoding.ASCII.GetString(state.buffer.Take(bytesRead).ToArray());
-                //   Dispatcher.Invoke(new Action(() => richTextBox1.Document.Blocks.Add(new Paragraph(new Run(ss)))), null);
+                var ss = Encoding.Default.GetString(state.buffer.Take(bytesRead).ToArray());
+                if (ss.Contains("HTTP/1.0 200 Connection established"))
+                {
+                    Dispatcher.Invoke(new Action(() =>
+                                                     {
+                                                         var uri = new Uri(textBox2.Text);
+                                                         Sock.Send(Encoding.Default.GetBytes(string.Format("GET {0} HTTP/1.1\r\nHost:{1}\r\n{2}\r\nProxy-Connection:keep-alive\r\n", uri.AbsolutePath, uri.Host, proxyAuth)));
+                                                         Sock.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+                                                     }), null);
+                    return;
+                }
+                var charset = Regex.Match(ss, @"charset=(?<charset>[\w-]*)", RegexOptions.IgnoreCase).Groups["charset"].Value;
+                if (!string.IsNullOrEmpty(charset))
+                    ss = Encoding.GetEncoding(charset).GetString(state.buffer.Take(bytesRead).ToArray());
+                Dispatcher.Invoke(new Action(() => richTextBox1.Document.Blocks.Add(new Paragraph(new Run(ss)))), null);
+                //Dispatcher.Invoke(new Action(() => richTextBox1.Document.Blocks.Add(new Paragraph(new Run(charset)))), null);
 
                 var title = Regex.Match(ss, @"<title>(?<title>.*)</title>", RegexOptions.IgnoreCase).Groups["title"].Value;
                 var doc = new Document(title, state.URL);
@@ -105,9 +120,9 @@ namespace _2SLab1
                 Model.Current.documents.Add(doc);
                 Model.Current.Documents = null;
                 Dispatcher.Invoke(new Action(() =>
-                                                       {
-                                                           listBox1.ItemsSource = Model.Current.Documents;
-                                                       }), null);
+                {
+                    listBox1.ItemsSource = Model.Current.Documents;
+                }), null);
             }
             catch (Exception exception)
             {
