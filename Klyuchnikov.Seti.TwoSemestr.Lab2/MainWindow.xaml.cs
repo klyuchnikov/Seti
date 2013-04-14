fba1ec2ba718723b7df47c55bedc2f2453d5c62d
Дмитрий Ключников <klyuchnikovds@gmail.com>
2013-04-14 17:58:51 +0400

    modify

--- Klyuchnikov.Seti.TwoSemestr.Lab2/MainWindow.xaml.cs
+++ Klyuchnikov.Seti.TwoSemestr.Lab2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,10 +14,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Klyuchnikov.Seti.TwoSemestr.CommonLibrary;
 
 namespace Klyuchnikov.Seti.TwoSemestr.Lab2
 {
+
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -29,7 +33,11 @@ namespace Klyuchnikov.Seti.TwoSemestr.Lab2
             InitializeComponent();
             var str = File.ReadAllText("urls.txt");
             textBox1.Text = str;
+            ThreadPool.SetMinThreads(10, 10);
+            ThreadPool.SetMaxThreads(10, 10);
+            label2.Content = 0;
         }
+
 
         private static Thread Th;
         /*    public static void Go(object data, bool timedOut)
@@ -54,33 +62,30 @@ namespace Klyuchnikov.Seti.TwoSemestr.Lab2
             //  Console.ReadLine();
             //  starter.Set();
             var arr = textBox1.Text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var count = (int)label2.Content;
+            count += arr.Length;
+            label2.Content = count;
+            if (arr.Any(s => !s.Contains("http://") && !s.Contains("https://")))
+            {
+                MessageBox.Show("Отсутствует префикс http://");
+                return;
+            }
             foreach (var s in arr)
             {
-                ThreadPool.QueueUserWorkItem(Delegate, s);
+                ThreadPool.QueueUserWorkItem(Task.Delegate, s);
             }
         }
-        void Delegate(object state)
-        {
-            Dispatcher.Invoke(new Action(() =>
-            {
-                listBox1.Items.Add(state);
-            }), null);
-            Parser.Start(state);
-        }
-
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-          /*  if (Th.ThreadState.HasFlag(ThreadState.WaitSleepJoin))
-                Th.Interrupt();
-            else
-                Th.Abort();//Sleep(Timeout.Infinite);
-            //   Th.*/
-            int nWorkerThreads;
-            int nCompletionThreads;
-            ThreadPool.GetMaxThreads(out nWorkerThreads, out nCompletionThreads);
-            MessageBox.Show("Максимальное количество потоков: " + nWorkerThreads
-                + "\nПотоков ввода-вывода доступно: " + nCompletionThreads);
+            foreach (Task task in Model2.Current.Tasks)
+                task.ThisThread.Resume();
+        }
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Task task in Model2.Current.Tasks)
+                task.ThisThread.Suspend();
         }
 
         private void button3_Click(object sender, RoutedEventArgs e)
@@ -88,5 +93,16 @@ namespace Klyuchnikov.Seti.TwoSemestr.Lab2
             var form = new ViewResual();
             form.ShowDialog();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (sender as Button);
+            var task = (button.DataContext as Task);
+            if (task.ThisThread.ThreadState.HasFlag(ThreadState.Suspended))
+                task.ThisThread.Resume();
+            else
+                task.ThisThread.Suspend();
+        }
+
     }
 }
