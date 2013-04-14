5d51db3617589468faf5d3da33255dae3f7ccf40
Дмитрий Ключников <klyuchnikovds@gmail.com>
2013-04-14 17:57:57 +0400

    new model2 and task for lab2

--- Seti.2SemestrLabs/Parser.cs
+++ Seti.2SemestrLabs/Parser.cs
@@ -48,38 +48,45 @@ namespace Klyuchnikov.Seti.TwoSemestr.CommonLibrary
 
         public static void Start(object state)
         {
-            var url = (string)state;
-            var Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            var uri = new Uri(url);
-            //     Sock.Connect("proxy.ustu", 3128);
-            Sock.Connect(uri.Host, 80);
-            //    Sock.Send(Encoding.Default.GetBytes("CONNECT users.ulstu.ru HTTP/1.0\r\n"));
-            Sock.Send(Encoding.Default.GetBytes(String.Format("GET {0} HTTP/1.1\r\nHost:{1}\r\n{2}\r\n", uri.AbsolutePath, uri.Host, proxyAuth)), SocketFlags.Partial);
-            //  Sock.Send(Encoding.Default.GetBytes("GET / HTTP/1.0\r\nHost:users.ulstu.ru\r\n\r\n" + proxyAuth));
-            Sock.ReceiveBufferSize = 5000000;
-            byte[] buffer = new byte[Sock.ReceiveBufferSize];
-            var bytesRead = 0;
-            var contentLenght = 0;
-            byte[] arr;
-            var str = "";
-            do
+            try
             {
-                if (bytesRead == 0)
-                    bytesRead += Sock.Receive(buffer);
-                else
-                    //   if (contentLenght - bytesRead ==)
-                    if (Sock.Available > 0)
-                        bytesRead += Sock.Receive(buffer, bytesRead, Sock.Available, SocketFlags.None);
-                // else
-                //      if (contentLenght - bytesRead < 20)
-                //          break;
-                arr = buffer.TakeWhile(q => q != 0).ToArray();
-                str = Encoding.Default.GetString(arr);
-            } while (!str.Contains("</html>"));
+                var url = (string)state;
+                var Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                var uri = new Uri(url);
+                //     Sock.Connect("proxy.ustu", 3128);
+                Sock.Connect(uri.Host, 80);
+                //    Sock.Send(Encoding.Default.GetBytes("CONNECT users.ulstu.ru HTTP/1.0\r\n"));
+                Sock.Send(Encoding.Default.GetBytes(String.Format("GET {0} HTTP/1.1\r\nHost:{1}\r\n{2}\r\n", uri.AbsolutePath, uri.Host, proxyAuth)), SocketFlags.Partial);
+                //  Sock.Send(Encoding.Default.GetBytes("GET / HTTP/1.0\r\nHost:users.ulstu.ru\r\n\r\n" + proxyAuth));
+                Sock.ReceiveBufferSize = 5000000;
+                byte[] buffer = new byte[Sock.ReceiveBufferSize];
+                var bytesRead = 0;
+                var contentLenght = 0;
+                byte[] arr;
+                var str = "";
+                do
+                {
+                    if (bytesRead == 0)
+                        bytesRead += Sock.Receive(buffer);
+                    else
+                        //   if (contentLenght - bytesRead ==)
+                        if (Sock.Available > 0)
+                            bytesRead += Sock.Receive(buffer, bytesRead, Sock.Available, SocketFlags.None);
+                    // else
+                    //      if (contentLenght - bytesRead < 20)
+                    //          break;
+                    arr = buffer.TakeWhile(q => q != 0).ToArray();
+                    str = Encoding.Default.GetString(arr);
+                } while (!str.Contains("</html>"));
 
-            arr = buffer.TakeWhile(q => q != 0).ToArray();
-            var ss = Encoding.Default.GetString(arr);
-            ParseDocument(arr, url);
+                arr = buffer.TakeWhile(q => q != 0).ToArray();
+                var ss = Encoding.Default.GetString(arr);
+                ParseDocument(arr, url);
+            }
+            catch (Exception exception)
+            {
+
+            }
         }
         public static void ParseDocument(byte[] arr, string url)
         {
---
+++ Seti.2SemestrLabs/Task.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace Klyuchnikov.Seti.TwoSemestr.CommonLibrary
+{
+    public class Model2 : INotifyPropertyChanged
+    {
+        private Model2()
+        {
+            tasks = new List<Task>();
+        }
+
+        public readonly List<Task> tasks;
+        public Task[] Tasks
+        {
+            get { return tasks.ToArray(); }
+            set { OnPropertyChanged("Tasks"); }
+        }
+        public static Model2 Current = new Model2();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+
+    public class Task : INotifyPropertyChanged
+    {
+        public Thread thisthread;
+        public Thread ThisThread
+        {
+            get { return thisthread; }
+        }
+
+        public ThreadState ThisThreadState
+        {
+            get
+            {
+                return thisthread != null ? thisthread.ThreadState : ThreadState.Unstarted;
+            }
+        }
+        public bool ThreadIsAlive
+        {
+            get
+            {
+                return thisthread != null && thisthread.IsAlive;
+            }
+        }
+
+        public string url;
+        public string URL
+        {
+            get { return url; }
+            set
+            {
+                url = value;
+                OnPropertyChanged("URL");
+            }
+        }
+        public static void Delegate(object state)
+        {
+            var url = (string)state;
+            var task =
+                Model2.Current.Tasks.SingleOrDefault(
+                    q => q.ThisThread.ManagedThreadId == Thread.CurrentThread.ManagedThreadId);
+            if (task == null)
+            {
+                task = new Task(url, Thread.CurrentThread);
+                Model2.Current.tasks.Add(task);
+                Model2.Current.OnPropertyChanged("Tasks");
+            }
+            task.URL = url;
+            /*   Dispatcher.Invoke(new Action(() =>
+               {
+                   listBox1.Items.Add(state);
+               }), null);*/
+            Parser.Start(url);
+        }
+
+        public Task(string url, Thread thread)
+        {
+            this.URL = url;
+            this.thisthread = thread;
+            var timer = new System.Timers.Timer(100);
+            timer.Elapsed += delegate { OnPropertyChanged("ThisThreadState"); OnPropertyChanged("ThreadIsAlive"); };
+            timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
