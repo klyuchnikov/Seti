589c87119ace7f020fd225987338f1f19d7c6904
Дмитрий Ключников <klyuchnikovds@gmail.com>
2013-04-14 17:56:41 +0400

    modify UI

---
+++ Klyuchnikov.Seti.TwoSemestr.Lab2/Converters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Data;
+
+namespace Klyuchnikov.Seti.TwoSemestr.Lab2
+{
+    class ThreadStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((ThreadState)value).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class ThreadStateButtonConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((ThreadState)value).HasFlag(ThreadState.Suspended) ? "Возобновить" : "Остановить";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
