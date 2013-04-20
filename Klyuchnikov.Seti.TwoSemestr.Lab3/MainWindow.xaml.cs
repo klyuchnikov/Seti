26251592a938bad9ba1146af588d4403f2d27863
Дмитрий Ключников <klyuchnikovds@gmail.com>
2013-04-20 15:50:28 +0400

    modify GUI

--- Klyuchnikov.Seti.TwoSemestr.Lab3/MainWindow.xaml.cs
+++ Klyuchnikov.Seti.TwoSemestr.Lab3/MainWindow.xaml.cs
@@ -27,7 +27,12 @@ namespace Klyuchnikov.Seti.TwoSemestr.Lab3
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             int port;
-            if (int.TryParse(portTB.Text, out port))
+            if (portTB.Text == "")
+            {
+                MessageBox.Show("Введите порт!");
+                return;
+            }
+            if (!int.TryParse(portTB.Text, out port))
             {
                 MessageBox.Show("Порт введен некорректно:");
                 return;
@@ -36,11 +41,13 @@ namespace Klyuchnikov.Seti.TwoSemestr.Lab3
             {
                 Server.Current.Start(port);
                 button1.Content = "Стоп";
+                portTB.IsEnabled = false;
             }
             else
             {
                 Server.Current.Stop();
                 button1.Content = "Старт";
+                portTB.IsEnabled = true;
             }
 
         }
